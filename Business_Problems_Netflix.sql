83deddaca60e36d20914d2d38cf3233e3b5a4a76
Rishabh Kumar <49457574+Rishabh45@users.noreply.github.com>
2024-11-18 11:38:20 +0530

    Add files via upload

---
+++ Business_Problems_Netflix.sql
@@ -0,0 +1,16 @@
+-- Business Problems of Netflix Movies and TV Shows.
+
+1. Find out the Number of Movies vs TV Shows Present on netflix?
+2. Find the Most Common Rating for Movies and TV Shows?
+3. Find out Most Number of Movies Released in a Specific Year (e.g., 2018)?
+4. Find Top 7 Countries with the Most Number of Movies and TV shows on Netflix?
+5. Identify the Longest Movie Duration?
+6. List All TV Shows with More Than  or Equal to 7 Seasons?
+7. Find Content Added in the Last 7 Years?
+8. Count the Number of Content in Each Genre?
+9. Find each year and the average numbers of content release in India on netflix?
+10. Find Out all the Movies that are Documentaries?
+11. Find All Content Without a Director?
+12. Find the Top 10 Actors Who Have Appeared in the Highest Number of Movies Produced in India?
+13. Find How Many Movies Actor 'Shah Rukh Khan' Appeared in the Netflix Last 5 Years?
+14. Categorize Content Based on the Presence of 'Kill' and 'Violence' Keywords.
